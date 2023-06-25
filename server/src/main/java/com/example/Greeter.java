bef72e27594983e64f88c350442e109508a1a10b
RamboKhanna <rajatkhannaca@gmail.com>
2023-06-25 10:36:30 -0400

    This is a new repo

---
+++ server/src/main/java/com/example/Greeter.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * This is a class.sdfsd
+ */
+public class Greeter {
+
+  /**
+   * This is a constructor classsssssggs .sssasdasds
+   */
+  public Greeter() {
+
+  }
+
+  //TODO: Add javadoc comment
+  public String greet(String someone) {
+    return String.format("Hello, %s!", someone);
+  }
+}
